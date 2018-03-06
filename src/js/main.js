df09cbdb9c82a8b765746f4f61269cabcf74eaeb
Marta Rico Solsona <martaricosolsona@gmail.com>
2018-03-06 19:33:36 +0100

    hago 1 commit

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+window.$ = window.jQuery = require("jquery");
+
+// $("header").on("click", function () {
+//     $(".web-header").css("background", "red");
+// });
+
+// $(".web-title").text("Hola");
+
+// $("button").attr("id", "boton");
+
+//$(".web-header").css("background", "red");
+
+// $(".web-header").addClass("cabecera");
+
+// $(".songs-list").removeClass("ideal").addClass("loading");
+
+
